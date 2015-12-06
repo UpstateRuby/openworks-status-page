2fab77270ce737c37eeffee2c279e675dd7769dc
Andrew Lechowicz <andrew@alecho.com>
2015-12-05 21:23:57 -0500

    Transition to location if only one is found

--- app/routes/locations.js
+++ app/routes/locations.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('location');
+  },
+  afterModel(model) {
+    if (model.get('length') === 1) {
+      this.transitionTo('location', model.get('firstObject'));
+    }
   }
 });
