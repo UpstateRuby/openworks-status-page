aea5c90d1bd22ff40baea43bc4e5d016843d354d
Rick Yoesting <rick.yoesting@gmail.com>
2016-04-27 20:38:22 -0400

    added the 'New' button locations/show; update locations-tests

--- tests/acceptance/locations-test.js
+++ tests/acceptance/locations-test.js
@@ -29,7 +29,7 @@ test('visiting /locations with exactly 1 location', function(assert) {
 
   andThen(function() {
     assert.equal(currentURL(), '/locations/1', 'Was not redirected to only location.');
-    assert.equal(find('h2').text(), 'Location 1');
+    assert.equal(find('.location-name').text(), 'Location 1');
   });
 });
 
@@ -41,5 +41,23 @@ test('visiting /locations with 2 location', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/locations', 'Should not be redirected');
     assert.equal(find('A:contains("Location")').length, 2);
+  });
+});
+
+test('clicking New button navigates to /things/new', function(assert) {
+  server.create('location');
+
+  const locationUrl = '/locations/1';
+  visit(locationUrl);
+
+  andThen(function() {
+    assert.equal(find('.location-name').text(), 'Location 1');
+    assert.equal(find('.btn-primary').text(), 'New');
+
+    click('.btn-primary');
+
+    andThen(function() {
+      assert.equal(currentURL(), '/things/new', 'Should have redirected');
+    });
   });
 });
