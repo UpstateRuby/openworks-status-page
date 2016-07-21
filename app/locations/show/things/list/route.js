cd8a4ca92b7bbf6fe0d223181644fd092df8106f
Andrew Lechowicz <andrew@alecho.com>
2016-07-21 09:30:45 -0400

    Use include param

--- app/locations/show/things/list/route.js
+++ app/locations/show/things/list/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.modelFor('locations/show').get('things');
+    return this.store.query('thing', {location_id: this.modelFor('locations/show').id,include: 'properties'});
   }
 });
